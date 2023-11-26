68a2b89f392f666c8fc0a2ed111c16caf4cb9a3d
Renato1006 <renato1006@gmail.com>
2023-11-25 21:52:47 -0300

    Initial commit

---
+++ src/main/java/br/com/renato/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,29 @@
+package br.com.renato.screenmatch;
+
+import br.com.renato.screenmatch.model.DadosSerie;
+import br.com.renato.screenmatch.service.ConsumoAPI;
+import br.com.renato.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sound.midi.Soundbank;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=7c41444");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
