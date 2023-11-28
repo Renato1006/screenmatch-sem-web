29f7bd045d3aab469b8b76703c82561325512174
Renato1006 <renato1006@gmail.com>
2023-11-27 21:30:09 -0300

    Limpando os imports não utilizados no run

--- src/main/java/br/com/renato/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/renato/screenmatch/ScreenmatchApplication.java
@@ -1,18 +1,9 @@
 package br.com.renato.screenmatch;
 
-import br.com.renato.screenmatch.model.DadosEpisodio;
-import br.com.renato.screenmatch.model.DadosSerie;
-import br.com.renato.screenmatch.model.DadosTemporada;
 import br.com.renato.screenmatch.principal.Principal;
-import br.com.renato.screenmatch.service.ConsumoAPI;
-import br.com.renato.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.sound.midi.Soundbank;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
